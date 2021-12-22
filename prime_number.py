e7141c4cd4d079616927a8b04b34f74fc32c3e03
ooooo5h <ooooo5h91@gmail.com>
2021-12-22 17:18:08 +0900

    나눠지는 숫자 발견시, 소수가 아니라고 처리함

--- prime_number.py
+++ prime_number.py
@@ -4,3 +4,7 @@ is_prime_num = True
 
 for i in range(2, num):
     # 2 ~ 입력한 숫자 직전까지 돌면서, 하나라도 나눠지면 소수가 아니라고 할 예정
+    if num % i == 0:
+        # 소수 발견!!
+        is_prime_num == False
+            
